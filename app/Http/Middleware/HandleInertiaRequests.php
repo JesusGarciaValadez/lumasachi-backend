91f7f4db91b0d5b6cafe8974e4e1b8849aabfc01
JesusGarciaValadez <jesus.garciav@me.com>
2025-07-25 16:53:25 -0600

    chore: update HandleInertiaRequests middleware and add Lumasachi app view
    - Changed the root view in HandleInertiaRequests middleware to 'lumasachi::app' for proper module integration.
    - Added a new Blade view file for the Lumasachi module, including dark mode support and necessary meta tags for the application layout.
    
    Signed-off-by: JesusGarciaValadez <jesus.garciav@me.com>

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -16,7 +16,7 @@ class HandleInertiaRequests extends Middleware
      *
      * @var string
      */
-    protected $rootView = 'app';
+    protected $rootView = 'lumasachi::app';
 
     /**
      * Determines the current asset version.
