3aabd18b2543372c9b581f415dbb109a2944f055
JesusGarciaValadez <jesus.garciav@me.com>
2025-08-28 14:22:39 -0600

    feat: enhance UserResource and update user retrieval endpoint
    - Added additional fields to UserResource including company UUID, company name, email verification timestamp, user type, preferences, phone number, and notes.
    - Updated the user retrieval endpoint to return a UserResource instance instead of a raw array, improving data structure and consistency.
    
    Signed-off-by: JesusGarciaValadez <jesus.garciav@me.com>

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,12 +16,19 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'company_uuid' => $this->whenNotNull($this->whenLoaded('company', fn($query) => $query->uuid)),
+            'company_name' => $this->whenNotNull($this->whenLoaded('company', fn($query) => $query->name)),
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'full_name' => $this->full_name,
             'email' => $this->email,
+            'email_verified_at' => $this->email_verified_at,
             'role' => $this->role,
+            'type' => $this->type,
             'is_active' => $this->is_active,
+            'preferences' => $this->preferences,
+            'phone_number' => $this->phone_number,
+            'notes' => $this->notes,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
