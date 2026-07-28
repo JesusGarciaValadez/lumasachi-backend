55b9bd407a1d47e9d7a6986dba359320737458e4
JesusGarciaValadez <jesus.garciav@me.com>
2026-07-28 16:50:08 -0600

    feat: set default locale variables for consistent test execution in CI
    Signed-off-by: JesusGarciaValadez <jesus.garciav@me.com>

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,10 +4,31 @@ declare(strict_types=1);
 
 use Tests\TestCase;
 
-pest()
-    ->tia()
-    ->always()
-    ->locally();
+$tiaPagesDirectory = getenv('TIA_VITE_PAGES_DIR');
+
+if (is_string($tiaPagesDirectory) && $tiaPagesDirectory !== '') {
+    $tiaPagesPath = str_starts_with($tiaPagesDirectory, DIRECTORY_SEPARATOR)
+        ? $tiaPagesDirectory
+        : dirname(__DIR__) . DIRECTORY_SEPARATOR . $tiaPagesDirectory;
+
+    if (!is_dir($tiaPagesPath)) {
+        putenv('TIA_VITE_PAGES_DIR=');
+    }
+}
+
+$pagesDirectory = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'js';
+$upperCasePagesDirectory = $pagesDirectory . DIRECTORY_SEPARATOR . 'Pages';
+$lowerCasePagesDirectory = $pagesDirectory . DIRECTORY_SEPARATOR . 'pages';
+$hasCaseInsensitivePagesMount = is_dir($upperCasePagesDirectory)
+    && is_dir($lowerCasePagesDirectory)
+    && fileinode($upperCasePagesDirectory) === fileinode($lowerCasePagesDirectory);
+
+if (!$hasCaseInsensitivePagesMount) {
+    pest()
+        ->tia()
+        ->always()
+        ->locally();
+}
 
 pest()
     ->extend(TestCase::class)
